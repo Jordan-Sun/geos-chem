28dbcb214b9768ee363fd8eaa48c7d35df85e374
Jordan Sun <zhuoran.sun@wustl.edu>
2024-11-26 16:15:16 -0600

    Removed debug print statements.

--- GeosCore/fullchem_mod.F90
+++ GeosCore/fullchem_mod.F90
@@ -1161,12 +1161,6 @@ CONTAINS
             RCNTRL_balanced(:,(I_CELL-1)*State_Grid%NX*State_Grid%NY+swap_indices(i)) = R_recv(:,(I_CELL-1)*NCELL_moving+i)
         end do
     end do
-
-    ! @Debug: Print out a copy of load-balanced data (C_balanced) a file named PET_<PET>.txt
-    IF (this_PET == 0) THEN
-        WRITE(10,*) 'Load-balanced data on PET ', this_PET, ': '
-        WRITE(10,*) C_balanced(:,:)
-    ENDIF
 #endif
 
     !$OMP PARALLEL DO                                                        &
@@ -1366,12 +1360,6 @@ CONTAINS
 
     ! Reverse the load balancing
 #ifdef MODEL_GCHPCTM
-    ! @Debug: Print out a copy of computed data (C_balanced) with PET number to a file named PET_<PET>.txt
-    IF (this_PET == 0) THEN
-        WRITE(10,*) 'Computed data on PET ', this_PET, ': '
-        WRITE(10,*) C_balanced(:, :)
-    ENDIF
-
     ! Gather the columns to be swapped to the *_recv arrays
     do I_CELL = 1, State_Grid%NZ
         do i = 1, NCELL_moving
@@ -1411,12 +1399,6 @@ CONTAINS
             RSTATE_1D(:,(I_CELL-1)*State_Grid%NX*State_Grid%NY+swap_indices(i)) = R_send(:,(I_CELL-1)*NCELL_moving+i)
         end do
     end do
-
-    ! @Debug: Print out a copy of reversed data (C_1D) with PET number to a file named PET_<PET>.txt
-    IF (this_PET == 0) THEN
-        WRITE(10,*) 'Reversed data on PET ', this_PET, ': '
-        WRITE(10,*) C_1D(:, :)
-    ENDIF
 #endif
     
     DO L = 1, State_Grid%NZ
@@ -3159,12 +3141,6 @@ CONTAINS
         NCELL_moving = NCELL_moving + 1
     end do
     NCELL_moving = NCELL_moving - 1  ! Length of swap_indices
-
-    ! Open the PET_0.txt file for writing only if this is PET 0
-    IF (Input_Opt%thisCPU == 0) THEN
-        OPEN(UNIT=10, FILE='PET_0.txt', STATUS='REPLACE', ACTION='WRITE')
-    ENDIF
-
   END SUBROUTINE Init_FullChem
 !EOC
 !------------------------------------------------------------------------------
@@ -3372,10 +3348,6 @@ CONTAINS
        CALL GC_CheckVar( 'fullchem_mod.F90:swap_indices', 2, RC )
        IF ( RC /= GC_SUCCESS ) RETURN
     ENDIF
-    
-    ! Close the PET_0.txt file, it won't have any effect if it is not open
-    CLOSE(UNIT=10)
-
   END SUBROUTINE Cleanup_FullChem
 !EOC
 END MODULE FullChem_Mod
